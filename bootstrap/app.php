0b629e3caae367ac26ccf3e2051e159a27cb68d1
Adler Parobocz <adlerdevfull@gmail.com>
2026-01-01 17:14:12 -0300

    chore: bootstrap Laravel API project scaffold

---
+++ bootstrap/app.php
@@ -0,0 +1,7 @@
+<?php
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Middleware;
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(api: __DIR__.'/../routes/api.php')
+    ->withMiddleware(function (Middleware $middleware) { $middleware->alias(['role'=>\Spatie\Permission\Middleware\RoleMiddleware::class]); $middleware->throttleApi('60,1'); })
+    ->withExceptions(function () {})->create();
